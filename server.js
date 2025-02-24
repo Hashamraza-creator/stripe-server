16ee43c64a30b49c0e4a2d86ec383112f38df70e
Hasham <mac@macs-MacBook-Pro.local>
2025-02-24 16:19:09 +0500

    6th commit

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ app.post('/charge', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
